8397add8a8f17380721673a902b0d6b480514b9f
Ronak <aboutalebi.ronak@gmail.com>
2026-08-15 21:07:26 +0330

    test projectile

---
+++ src/main/java/views/graphical/gameplay/actors/ProjectileActor.java
@@ -0,0 +1,139 @@
+package views.graphical.gameplay.actors;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import graphics.PvzGame;
+import models.Plant.PlantTag;
+import models.projectile.Projectile;
+import views.graphical.animation.PamAnimationActor;
+
+public class ProjectileActor extends Group {
+
+    private static final String PEA_PAM =
+            "768/INITIAL/EFFECTS/SLINGPEA_PROJECTILE/SLINGPEA_PROJECTILE.PAM";
+
+    private static final String PEA_CLIP =
+            "tier1";
+
+    private static final String CABBAGE_PAM =
+            "768/INITIAL/EFFECTS/CABBAGEPULT_PLANTFOOD_PROJECTILE/CABBAGEPULT_PLANTFOOD_PROJECTILE.PAM";
+
+    private static final String CABBAGE_CLIP =
+            "plantfood_cabbage";
+
+    private static final float PEA_SCALE = 1;
+    private static final float CABBAGE_SCALE = 1;
+
+    private final Projectile projectile;
+    private final PamAnimationActor animation;
+
+    public ProjectileActor(
+            PvzGame game,
+            Projectile projectile
+    ) {
+        this.projectile = projectile;
+
+        setTransform(true);
+        setTouchable(Touchable.disabled);
+
+        String pamPath =
+                resolvePamPath(projectile);
+
+        String clip =
+                resolveClip(projectile);
+
+        game.getPamPlayer().loadSync(
+                pamPath
+        );
+
+        animation =
+                game.createPamActor(
+                        pamPath,
+                        clip,
+                        0f,
+                        0f,
+                        true
+                );
+
+        animation.setScale(
+                resolveScale(projectile)
+        );
+
+        animation.setTouchable(
+                Touchable.disabled
+        );
+        Image originMarker =
+                new Image(
+                        game.getSkin()
+                                .newDrawable(
+                                        "white_pixel",
+                                        Color.RED
+                                )
+                );
+
+        originMarker.setBounds(
+                -3f,
+                -3f,
+                6f,
+                6f
+        );
+
+        originMarker.setTouchable(
+                Touchable.disabled
+        );
+
+        addActor(originMarker);
+        addActor(animation);
+
+        addActor(animation);
+    }
+
+    public void setProjectilePosition(
+            float x,
+            float y
+    ) {
+        setPosition(
+                x,
+                y
+        );
+    }
+
+    private String resolvePamPath(
+            Projectile projectile
+    ) {
+        if (isCabbageTestProjectile(projectile)) {
+            return CABBAGE_PAM;
+        }
+
+        return PEA_PAM;
+    }
+
+    private String resolveClip(
+            Projectile projectile
+    ) {
+        if (isCabbageTestProjectile(projectile)) {
+            return CABBAGE_CLIP;
+        }
+
+        return PEA_CLIP;
+    }
+
+    private float resolveScale(
+            Projectile projectile
+    ) {
+        if (isCabbageTestProjectile(projectile)) {
+            return CABBAGE_SCALE;
+        }
+
+        return PEA_SCALE;
+    }
+
+    private boolean isCabbageTestProjectile(
+            Projectile projectile
+    ) {
+        return projectile.getTargetX() != null
+                && projectile.getTargetY() != null;
+    }
+}
---
+++ src/main/java/views/graphical/gameplay/manager/ProjectileViewManager.java
@@ -0,0 +1,135 @@
+package views.graphical.gameplay.manager;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+import graphics.PvzGame;
+import models.projectile.Projectile;
+import views.graphical.gameplay.actors.ProjectileActor;
+import views.graphical.gameplay.board.BoardArea;
+import views.graphical.gameplay.board.BoardTransform;
+
+import java.util.Collections;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class ProjectileViewManager extends Group {
+
+    private final PvzGame game;
+    private final BoardTransform transform;
+
+    private final Map<Projectile, ProjectileActor>
+            projectileActors =
+            new IdentityHashMap<>();
+
+    public ProjectileViewManager(
+            PvzGame game,
+            BoardTransform transform
+    ) {
+        this.game = game;
+        this.transform = transform;
+    }
+
+    public void sync(
+            Iterable<Projectile> projectiles
+    ) {
+        Set<Projectile> active =
+                Collections.newSetFromMap(
+                        new IdentityHashMap<>()
+                );
+
+        for (Projectile projectile : projectiles) {
+
+            if (projectile.isMarkedForRemoval()) {
+                continue;
+            }
+
+            active.add(
+                    projectile
+            );
+
+            ProjectileActor actor =
+                    projectileActors.get(
+                            projectile
+                    );
+
+            if (actor == null) {
+
+                actor =
+                        new ProjectileActor(
+                                game,
+                                projectile
+                        );
+
+                projectileActors.put(
+                        projectile,
+                        actor
+                );
+
+                addActor(actor);
+            }
+
+            positionProjectile(
+                    projectile,
+                    actor
+            );
+        }
+
+        removeMissingProjectiles(
+                active
+        );
+    }
+
+    private void positionProjectile(
+            Projectile projectile,
+            ProjectileActor actor
+    ) {
+        BoardArea area =
+                transform.getArea();
+
+        float x =
+                area.x()
+                        + ((float) projectile.getPosX() + 0.5f)
+                        * transform.tileWidth();
+
+        float y =
+                area.y()
+                        + (
+                        BoardTransform.ROWS
+                                - 1f
+                                - (float) projectile.getPosY()
+                                + 0.5f
+                )
+                        * transform.tileHeight();
+
+        actor.setProjectilePosition(
+                x,
+                y
+        );
+    }
+
+    private void removeMissingProjectiles(
+            Set<Projectile> active
+    ) {
+        Iterator<Map.Entry<Projectile, ProjectileActor>>
+                iterator =
+                projectileActors
+                        .entrySet()
+                        .iterator();
+
+        while (iterator.hasNext()) {
+
+            Map.Entry<Projectile, ProjectileActor>
+                    entry =
+                    iterator.next();
+
+            if (!active.contains(
+                    entry.getKey()
+            )) {
+
+                entry.getValue().remove();
+                iterator.remove();
+            }
+        }
+    }
+}
